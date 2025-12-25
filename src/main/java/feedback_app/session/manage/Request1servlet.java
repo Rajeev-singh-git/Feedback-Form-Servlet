df8ba7a6cc03acd8d67ae71a5c5286c4ba3b6183
Rajeev  Singh <rajeevsinghece49@gmail.com>
2025-12-25 20:24:43 +0530

    Servlet code corrected

--- src/main/java/feedback_app/session/manage/Request1servlet.java
+++ src/main/java/feedback_app/session/manage/Request1servlet.java
@@ -14,40 +14,14 @@ public class Request1servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		resp.setContentType("text/html");
-		var writer = resp.getWriter();
-		Cookie[] cookies = req.getCookies();
 		
-		var flag = false;
-		
-		
-		if(cookies!=null) {
-			
-			for(Cookie c:cookies) {
-				String name = c.getName();
-				if(name.equals("username")) {
-					System.out.println("username : " + c.getValue());
-					writer.println("""
-							
-							 	 <h1>Welcome : %s</h1>
-							
-							     """.formatted(c.getValue()));
-					flag = true;
-				}
-				if ("JSESSIONID".equals(c.getName())) {
-			            String jsessionId = c.getValue();
-			            System.out.println("JSESSIONID : " + jsessionId);
-			     }
-			}		
-		}
-		
-		
-		if(!flag) {
-			writer.println("<h1>No username found in cookies</h1>");
-			writer.println("<h1>Username cookie is not there in request</h1>");
-		}
-		
+		 String username = "durgesh";
+		 
+		 Cookie cookie = new Cookie("username", username);
+		 
+		 cookie.setMaxAge(10*60);
+		 
+		 resp.addCookie(cookie);
 	}
 	
 	
--- src/main/java/feedback_app/session/manage/Request2Servlet.java
+++ src/main/java/feedback_app/session/manage/Request2Servlet.java
@@ -12,15 +12,38 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/servlet2")
 public class Request2Servlet extends HttpServlet {
 	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		resp.setContentType("text/html");
+		var writer = resp.getWriter();
+		Cookie[] cookies = req.getCookies();
 		
-		 String username = "durgesh";
-		 
-		 Cookie cookie = new Cookie("username", username);
-		 
-		 cookie.setMaxAge(10*60);
-		 resp.addCookie(cookie);
+		var flag = false;
+		
+		
+		if(cookies!=null) {
+			
+			for(Cookie c:cookies) {
+				String name = c.getName();
+				if(name.equals("username")) {
+					System.out.println("username : " + c.getValue());
+					writer.println("""
+							
+							 	 <h1>Welcome : %s</h1>
+							
+							     """.formatted(c.getValue()));
+					flag = true;
+				}
+			}		
+		}
+		
+		
+		if(!flag) {
+			writer.println("<h1>No username found in cookies</h1>");
+			writer.println("<h1>Username cookie is not there in request</h1>");
+		}
+		
 	}
-
 }
